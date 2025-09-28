e51b42f92fd14a017d3aa777e970d8fcb6067106
Neo-996 <abdulelahmalshehri@gmail.com>
2025-09-28 12:08:35 -0700

    Create 0-fizzbuzz.py

---
+++ challenge/0-fizzbuzz.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" FizzBuzz
+"""
+import sys
+
+
+def fizzbuzz(n):
+    """
+   The FizzBuzz function outputs numbers from 1 to n, separated by spaces.
+   - If a number is divisible by 3, it prints "Fizz" instead.
+   - If a number is divisible by 5, it prints "Buzz" instead.
+   - If a number is divisible by both 3 and 5, it prints "FizzBuzz".
+    """
+    if n < 1:
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
